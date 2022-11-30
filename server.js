8393354f5ae9eb8867a07bf17e02bec31317717d
elystanz <elysestanziale@gmail.com>
2022-11-30 16:12:39 -0500

    changed port to dynamic

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 // const port = 3001;
-const port = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 
 const app = express();
 
@@ -15,5 +15,5 @@ app.get("*", (req, res) => {
 })
 
 app.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}`)
+    console.log(`Server is listening on port ${PORT}`)
 });
