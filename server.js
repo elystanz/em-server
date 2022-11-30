b460bb5c48bebd468b5960e1647ff645f892f268
elystanz <elysestanziale@gmail.com>
2022-11-30 16:13:15 -0500

    spelling error in server

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, 'DevFolio/index.html'))
 })
 
-app.listen(port, ()=>{
+app.listen(PORT, ()=>{
     console.log(`Server is listening on port ${PORT}`)
 });
